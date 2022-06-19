718022a74219b1c87ecaa5f6d06ca137897451de
Zethus <ravet.zvika93@gmail.com>
2022-06-19 13:36:50 +0300

    cors + api base uri change

--- server.js
+++ server.js
@@ -3,18 +3,26 @@
 const express = require('express')
 const app = express()
 const PORT = 3000
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const products = require('./src/routes/products')
 const manufacturers = require('./src/routes/manufacturers')
 
+
+app.use(cors({
+    origin: 'http://localhost:4200'
+}));
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     // handle root
 })
 
 
-app.use('/products', products)
-app.use('/manufacturers', manufacturers)
+app.use('/api/products', products)
+app.use('/api/manufacturers', manufacturers)
 
 
 app.listen(PORT)
