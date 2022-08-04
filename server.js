0f2978168f4b3dd23b607648c4f55029f63de140
Zethus <ravet.zvika93@gmail.com>
2022-08-04 12:59:26 +0300

    comments

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const bodyParser = require('body-parser');
 
 const products = require('./src/routes/products')
 const manufacturers = require('./src/routes/manufacturers')
+const branches = require('./src/routes/branches')
 
 
 app.use(cors({
@@ -16,20 +17,22 @@ app.use(cors({
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    // handle root
-})
+// handle root
+app.get('/', (req, res) => {})
+
 
 app.use('/api/products', products)
 app.use('/api/manufacturers', manufacturers)
+app.use('/api/branches', branches)
 
-
+// socket config
 const socket = require('socket.io')(3210, {
     cors: {
         origin: '*',
     }
 })
 
+// transfer socket to routes
 app.set('socket', socket)
 
 
