d3ccf3788153ad57b8895ab42cf49e964cc1f803
Zethus <ravet.zvika93@gmail.com>
2022-08-05 12:31:01 +0300

    branch delete component

--- src/app/components/delete-branch/delete-branch/delete-branch.component.ts
+++ src/app/components/delete-branch/delete-branch/delete-branch.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { BranchService } from 'src/app/services/branch/branch.service';
+import { BranchesComponent } from '../../branches/branches/branches.component';
 
 @Component({
   selector: 'app-delete-branch',
@@ -7,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeleteBranchComponent implements OnInit {
 
-  constructor() { }
+  branch!: any
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any, 
+  private branchService: BranchService, 
+  private dialogRef: MatDialogRef<BranchesComponent>) { }
 
   ngOnInit(): void {
+    this.branch = this.data.branch
   }
 
+  deleteProduct() {
+    this.branchService.deleteBranch(this.branch._id)
+    this.dialogRef.close({event: 'Deleted'})
+  }
 }
