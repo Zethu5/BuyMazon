9586593c308b1738f08195535e2f599cd1de6377
Zethus <ravet.zvika93@gmail.com>
2022-08-05 13:15:07 +0300

    added update branch component

--- src/app/components/update-branch/update-branch/update-branch.component.ts
+++ src/app/components/update-branch/update-branch/update-branch.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  Inject,
+  OnInit
+} from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Branch } from 'src/app/models/branch';
+import { BranchService } from 'src/app/services/branch/branch.service';
+import { BranchesComponent } from '../../branches/branches/branches.component';
 
 @Component({
   selector: 'app-update-branch',
@@ -7,9 +16,78 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateBranchComponent implements OnInit {
 
-  constructor() { }
+  updateBranchForm!: FormGroup
+  branch!: any
+  cities = [
+    'ירושלים',
+    'תל אביב',
+    'חיפה',
+    'ראשון לציון',
+    'פתח תקווה',
+    'אשדוד',
+    'נתניה',
+    'באר שבע',
+    'בני ברק',
+    'חולון',
+    'רמת גן',
+    'אשקלון',
+    'רחובות',
+    'בית שמש',
+    'בת ים',
+    'כפר סבא',
+    'הרצליה',
+    'חדרה',
+    'מודיעין-מכבים-רעות',
+    'לוד'
+  ]
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any,
+    private fb: FormBuilder,
+    private branchService: BranchService,
+    private dialogRef: MatDialogRef < BranchesComponent > , ) {}
 
   ngOnInit(): void {
+    this.branch = this.data.branch
+
+    this.updateBranchForm = this.fb.group({
+      city: [this.branch.city, [Validators.required]],
+      address: [this.branch.address, [Validators.required]],
+      phone: [this.branch.phone, [Validators.required, this.isPhone]],
+      picture: [this.branch.picture, [Validators.required, this.isImageURI]],
+    })
   }
 
+  isImageURI(control: AbstractControl) {
+    let imageRegex = /(https?:\/\/.*\.(?:png|jpg))/i
+    if (!imageRegex.test(control.value)) {
+      return { invalidImageURI: true }
+    }
+    return null
+  }
+
+  isPhone(control: AbstractControl) {
+    let phoneRegex = /^0[1-9]\-\d{7}$/
+
+    if (!phoneRegex.test(control.value)) {
+      return { invalidPhoneNumber: true }
+    }
+    return null
+  }
+
+  onBranchSubmit() {
+    if(!this.updateBranchForm.valid) return
+
+    let updatedBranch: Branch = {
+      city: this.updateBranchForm.controls['city'].value,
+      address: this.updateBranchForm.controls['address'].value,
+      phone: this.updateBranchForm.controls['phone'].value,
+      picture: this.updateBranchForm.controls['picture'].value,
+    }
+
+    this.branchService.updateBranch(this.branch._id, updatedBranch)
+
+    this.dialogRef.close({
+      event: 'Updated'
+    })
+  }
 }
