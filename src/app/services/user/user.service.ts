20230c005d912c76cee79b072488c7a7602ce924
Zethus <ravet.zvika93@gmail.com>
2022-08-08 13:48:58 +0300

    user service

---
+++ src/app/services/user/user.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { User } from 'src/app/models/user';
+import { api_base } from '../../../environments/environment'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  constructor(private http: HttpClient) {}
+
+  getUsers() {
+    const url = `${api_base}/users`
+    return this.http.get(url)
+  }
+
+  createUser(user: User) {
+    const url = `${api_base}/users`
+    this.http.post(url, user).subscribe()
+  }
+
+  getUser(id: string) {
+    const url = `${api_base}/users/${id}`
+    return this.http.get(url)
+  }
+
+  deleteUser(id: string) {
+    const url = `${api_base}/users/${id}`
+    this.http.delete(url).subscribe()
+  }
+
+  updateUser(id: string, user: User) {
+    const url = `${api_base}/users/${id}`
+    this.http.put(url, user).subscribe()
+  }
+}
