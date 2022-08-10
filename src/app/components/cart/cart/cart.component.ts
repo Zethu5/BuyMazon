8e1e7b94c1b3ac38929698fea7a38f32556407d4
Zethus <ravet.zvika93@gmail.com>
2022-08-10 18:08:39 +0300

    implemeneted the cart component

--- src/app/components/cart/cart/cart.component.ts
+++ src/app/components/cart/cart/cart.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Product } from 'src/app/models/product';
+import { User } from 'src/app/models/user';
+import { CartService } from 'src/app/services/cart/cart.service';
+import { ProductService } from 'src/app/services/product/product.service';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-cart',
@@ -7,9 +13,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
-  constructor() { }
+  user!: any
+  products: [Product?] = []
+  displayedColumns: string[] = ['name', 'picture', 'code', 'price', 'weight', 'manufacturer', 'times'];
+
+  constructor(
+    private userService: UserService, 
+    private cartService: CartService, 
+    private productService: ProductService,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
+    this.userService.getUserByUsername(this.userService.getLocalStorageUserName()!).subscribe((user: any) => {
+      this.user = user
+      this.cartService.getUserCartById(this.user._id).subscribe((cart: any) => {
+        const cartProductIds = cart.map((x: any) => x._id)
+        this.productService.getProducts().subscribe((data: any) => {
+          this.products = data.filter((x: any) => cartProductIds.includes(x._id))
+          this.products.forEach((element: any) => {
+            element.times = cart.filter((x: any) => element._id === x._id)[0].value
+          });
+        })
+      })
+    })
   }
 
 }
