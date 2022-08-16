add619b7407ad688d1c08cd5124177137b131c91
Zethus <ravet.zvika93@gmail.com>
2022-08-17 00:19:11 +0300

    tidy up code

--- src/app/components/statistics/statistics/statistics.component.ts
+++ src/app/components/statistics/statistics/statistics.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { min } from 'rxjs';
 import * as io from 'socket.io-client';
-import { Order } from 'src/app/models/order';
-import { Product } from 'src/app/models/product';
 import { OrderService } from 'src/app/services/order/order.service';
 import { ProductService } from 'src/app/services/product/product.service';
 import { socket_connection } from '../../../../environments/environment';
@@ -15,75 +12,78 @@ import { socket_connection } from '../../../../environments/environment';
 export class StatisticsComponent implements OnInit {
 
   socket!: any
-  bounds:number[][] = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
-  constructor(private orderService:OrderService, private productService:ProductService ) { }
+  bounds: number[][] = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]
+  ]
+
+  constructor(
+    private orderService: OrderService,
+    private productService: ProductService
+  ) {}
 
   ngOnInit(): void {
-    this.updateUpperBound()
     this.initUpperBound()
-    this.returnUppBoundJSON()
+    this.updateUpperBound()
   }
-  getUpperBoundforProdCode(id:any){
-    let h1=this.bounds[0][this.hash1(id)]
-    let h2=this.bounds[1][this.hash1(id)]
-    let h3=this.bounds[2][this.hash1(id)]
-    let arr:Array<number> = [h1,h2,h3]
+
+  getUpperBoundforProdCode(id: any) {
+    let h1 = this.bounds[0][this.hash1(id)]
+    let h2 = this.bounds[1][this.hash1(id)]
+    let h3 = this.bounds[2][this.hash1(id)]
+    let arr: Array < number > = [h1, h2, h3]
     return Math.max(...arr)
   }
+
   initUpperBound() {
-    this.orderService.getOrders().subscribe((data:any)=>{
-      data.forEach((order:any)=>{
-        order.products.forEach((prod:any) => {
+    this.orderService.getOrders().subscribe((data: any) => {
+      data.forEach((order: any) => {
+        order.products.forEach((prod: any) => {
           let id = prod.code
-          this.bounds[0][this.hash1(id)]+=prod.amount
-          this.bounds[1][this.hash2(id)]+=prod.amount
-          this.bounds[2][this.hash3(id)]+=prod.amount
-         });
-      });
-      console.log(this.bounds)
-    })
-  }
- 
-  
-  updateUpperBound() {
-    
-    this.socket = io.io(socket_connection)
-    this.socket.on('newOrder', (order:any) => {
-      
-      order.products.forEach((prod:any) => {
-        
-      let id = prod.code
-      this.bounds[0][this.hash1(id)]+=prod.amount
-      this.bounds[1][this.hash2(id)]+=prod.amount
-      this.bounds[2][this.hash3(id)]+=prod.amount
-     });
-     
-    })
-  }
-  // Use this to get a upper bound for every product json
-  returnUppBoundJSON(){
-    this.productService.getProducts().subscribe((data:any)=>{
-      let boundsArray:any = []
-      data.forEach((product:any)=>{
-        boundsArray.push({
-          product:product,
-          upperBound:this.getUpperBoundforProdCode(product.code)
+          this.bounds[0][this.hash1(id)] += prod.amount
+          this.bounds[1][this.hash2(id)] += prod.amount
+          this.bounds[2][this.hash3(id)] += prod.amount
         })
       })
-      console.log(boundsArray)
     })
-
   }
+
+  updateUpperBound() {
+    this.socket = io.io(socket_connection)
+    this.socket.on('newOrder', (order: any) => {
+      order.products.forEach((prod: any) => {
+        let id = prod.code
+        this.bounds[0][this.hash1(id)] += prod.amount
+        this.bounds[1][this.hash2(id)] += prod.amount
+        this.bounds[2][this.hash3(id)] += prod.amount
+      })
+    })
+  }
+
+  // Use this to get a upper bound for every product json
+  returnUppBoundJSON() {
+    this.productService.getProducts().subscribe((data: any) => {
+      let boundsArray: any = []
+      data.forEach((product: any) => {
+        boundsArray.push({
+          product: product,
+          upperBound: this.getUpperBoundforProdCode(product.code)
+        })
+      })
+    })
+  }
+
   // CMS hash functions
-  hash1(input: number){
-    return input%5
-  }
-  hash2(input: number){
-    return (input*7)%5
-  }
-  hash3(input: number){
-    return (input*2)%5
+  hash1(input: number) {
+    return input % 5
   }
 
+  hash2(input: number) {
+    return (input * 7) % 5
+  }
 
+  hash3(input: number) {
+    return (input * 2) % 5
+  }
 }
