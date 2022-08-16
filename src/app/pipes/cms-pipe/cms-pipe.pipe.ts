730f8110355bfa981fc533fe7dad174e1b508d58
Zethus <ravet.zvika93@gmail.com>
2022-08-17 02:12:13 +0300

    custom pipe using the cms service

--- src/app/pipes/cms-pipe/cms-pipe.pipe.ts
+++ src/app/pipes/cms-pipe/cms-pipe.pipe.ts
@@ -1,12 +1,16 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { lastValueFrom, map, of } from 'rxjs';
+import { CMSService } from 'src/app/services/cms/cms.service';
 
 @Pipe({
   name: 'cmsPipe'
 })
 export class CmsPipePipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  constructor(private cmsService: CMSService) {}
+
+  async transform(id: string, ...args: unknown[]) {
+    return await lastValueFrom(this.cmsService.getUpperBoundforProdCode(id))
   }
 
 }
