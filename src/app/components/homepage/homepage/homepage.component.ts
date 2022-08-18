725a709c7f3edb3de609dc6af3d2dd4cb5e65c9f
Zethus <ravet.zvika93@gmail.com>
2022-08-18 13:11:47 +0300

    organized categories on user admin/regular user

--- src/app/components/homepage/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage/homepage.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { AdService } from 'src/app/services/ad/ad.service';
 import * as io from 'socket.io-client';
 import { socket_connection } from 'src/environments/environment';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-homepage',
@@ -17,6 +18,7 @@ export class HomepageComponent implements OnInit {
 
   constructor(
     private adService: AdService,
+    private userService: UserService,
     private router: Router
   ) { }
 
@@ -24,31 +26,36 @@ export class HomepageComponent implements OnInit {
     this.categories.push({
       name: 'Manufacturers',
       image: 'https://i.imgur.com/kjLiqN6.png',
-      ref: '/manufacturers'
+      ref: '/manufacturers',
+      allowed: ['admins']
     })
 
     this.categories.push({
       name: 'Branches',
       image: 'https://i.imgur.com/UyZ5rBN.png',
-      ref: '/branches'
+      ref: '/branches',
+      allowed: ['users', 'admins']
     })
 
     this.categories.push({
       name: 'Products',
       image: 'https://i.imgur.com/FdDTEXb.png',
-      ref: '/products'
+      ref: '/products',
+      allowed: ['users', 'admins']
     })
 
     this.categories.push({
       name: 'Statistics',
       image: 'https://i.imgur.com/j8RnXQh.png',
-      ref: '/statistics'
+      ref: '/statistics',
+      allowed: ['admins']
     })
 
     this.categories.push({
       name: 'Ads',
       image: 'https://i.imgur.com/AiZmuRZ.png',
-      ref: '/ads'
+      ref: '/ads',
+      allowed: ['users', 'admins']
     })
 
     this.getAds()
@@ -76,4 +83,16 @@ export class HomepageComponent implements OnInit {
     return (new Date(date)).toLocaleDateString()
   }
 
+  isLoggedIn() {
+    return this.userService.isLoggedIn()
+  }
+
+  isAdmin() {
+    return this.userService.isAdmin()
+  }
+
+  isAllowedToDisplayCategory(category: any) {
+    if(category.allowed.includes('users')) return true
+    return (this.isAdmin() ? true : false)
+  }
 }
