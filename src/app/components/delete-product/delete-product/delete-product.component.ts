5af923a70cd9a5799d9ba120945a1bb98f7d34c2
Zethus <ravet.zvika93@gmail.com>
2022-08-19 13:03:27 +0300

    deleting product will delete it from ads

--- src/app/components/delete-product/delete-product/delete-product.component.ts
+++ src/app/components/delete-product/delete-product/delete-product.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { AdService } from 'src/app/services/ad/ad.service';
 import { CartService } from 'src/app/services/cart/cart.service';
 import { ProductService } from 'src/app/services/product/product.service';
 import { UserService } from 'src/app/services/user/user.service';
@@ -18,6 +19,7 @@ export class DeleteProductComponent implements OnInit {
   private productService: ProductService,
   private userService: UserService,
   private cartService: CartService,
+  private adService: AdService,
   private dialogRef: MatDialogRef<ProductsComponent>) { }
 
   ngOnInit(): void {
@@ -26,14 +28,22 @@ export class DeleteProductComponent implements OnInit {
 
   deleteProduct() {
     this.userService.getUsers().subscribe((userData: any) => {
-      // clear carts of the product
-      userData.forEach((user: any) => {
-        this.cartService.removeBulkProductsFromUser(user, [this.product])
-      })
+      this.adService.getAds().subscribe((adData: any) => {
+        // clear carts from the product
+        userData.forEach((user: any) => {
+          this.cartService.removeBulkProductsFromUser(user, [this.product])
+        })
 
-      // delete product
-      this.productService.deleteProduct(this.product._id)
-      this.dialogRef.close({event: 'Deleted'})
+        // clear ads from the product
+        adData.forEach((ad: any) => {
+          ad.products = ad.products.filter((p: any) => p._id != this.product._id)
+          this.adService.updateAd(ad._id, ad)
+        })
+
+        // delete product
+        this.productService.deleteProduct(this.product._id)
+        this.dialogRef.close({event: 'Deleted'})
+      })
     })
   }
 }
